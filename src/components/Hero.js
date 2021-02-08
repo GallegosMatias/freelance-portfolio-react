e6d3f306f41ee5b20a288cb1180a20340bcc7d86
jhed <gallegosmatiasjoel@gmail.com>
2021-02-08 12:04:42 -0300

    Hero Component Structure

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <div>
+            <section id="hero">
+                <div class="hero container">
+                <div class="">
+                    <h1>Hola,<span></span></h1>
+                    <h1>Mi nombre es<span></span></h1>
+                    <h1>Matías<span></span></h1>
+                    <a href="#projects" type="button" class="cta">Portafolio</a>
+                </div>
+                </div>
+            </section>          
+        </div>
+    )
+}
+
+export default Hero
