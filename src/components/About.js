85038e5fda52e3d3083e968558212690d964aaf6
jhed <gallegosmatiasjoel@gmail.com>
2021-02-08 12:10:54 -0300

    About Component Structure

---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <section id="about">
+                <div class="about container">
+
+                <div class="about-left">
+                    <div class="about-left-img">
+                    <img src="./img/img-2.png" alt="" />
+                    </div>
+                </div>
+
+                <div class="about-right">
+                    <h1 class="section-title">Sobre <span>mí</span></h1>
+                    <h2>Front End Developer</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia suscipit magni quasi enim culpa quod quisquam odio quam doloribus, aut voluptates doloremque tempore vel maxime, possimus esse sint veniam maiores.</p>
+                    <a href="https://drive.google.com/file/d/1Jb0bizlvSeGbV0cqGhEIiXOxPkFx1s0y/view?usp=sharing" target="_blank">Descargar CV</a>
+                </div>
+
+                </div>
+            </section>       
+        </div>
+    )
+}
+
+export default About
