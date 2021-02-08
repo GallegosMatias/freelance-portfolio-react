7e6dd10325e4078e3f2d524634c8259e86fcb221
jhed <gallegosmatiasjoel@gmail.com>
2021-02-08 12:11:59 -0300

    Services Component Structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
-
+import Services from './components/Services';
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <About />
+      <Services />
     </div>
   );
 } 
---
+++ src/components/Services.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <div>
+            <section id="services">
+                <div class="services container">
+
+                <div class="services-top">
+                    <h1 class="section-title">Servicios</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad iste quae voluptatum, perspiciatis necessitatibus numquam officiis placeat, inventore. Nemo incidunt, laboriosam distinctio. Magnam ipsam adipisci blanditiis reprehenderit libero repellendus excepturi?</p>
+                </div>
+
+                <div class="services-bottom">
+
+                    <div class="services-item">
+                    <div class="icon">
+                        <img src="img/design.png" alt="" />
+                    </div>
+                    <h2>Web Design</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos numquam, maiores a ducimus labore laborum, dicta rerum dolores. Asperiores cumque fuga odit, perspiciatis commodi fugit dolore, itaque possimus illum impedit?</p>
+                    </div>
+
+                    <div class="services-item">
+                    <div class="icon">
+                        <img src="img/dev.png" alt="" />
+                    </div>
+                    <h2>Web Development</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos numquam, maiores a ducimus labore laborum, dicta rerum dolores. Asperiores cumque fuga odit, perspiciatis commodi fugit dolore, itaque possimus illum impedit?</p>
+                    </div>
+
+                    <div class="services-item">
+                    <div class="icon">
+                        <img src="img/mobile.png" alt="" />
+                    </div>
+                    <h2>Responsive Design</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos numquam, maiores a ducimus labore laborum, dicta rerum dolores. Asperiores cumque fuga odit, perspiciatis commodi fugit dolore, itaque possimus illum impedit?</p>
+                    </div>
+
+                    <div class="services-item">
+                    <div class="icon">
+                        <img src="img/design.png" alt="" />
+                    </div>
+                    <h2>Web Design</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos numquam, maiores a ducimus labore laborum, dicta rerum dolores. Asperiores cumque fuga odit, perspiciatis commodi fugit dolore, itaque possimus illum impedit?</p>
+                    </div>
+                </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Services
