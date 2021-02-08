77871458e681fe9c6751c31b81bedad84e1fa409
jhed <gallegosmatiasjoel@gmail.com>
2021-02-08 12:14:44 -0300

    Projects Component Structure

---
+++ src/components/Projects.js
@@ -0,0 +1,84 @@
+import React from 'react'
+
+function Projects() {
+    return (
+        <div>
+            <section id="projects">
+
+            <div class="projects container">
+
+            <div class="projects-top">
+                <h1 class="section-title"><span>Proyectos</span> Recientes</h1>
+            </div>
+
+            <div class="projects-bottom">
+
+                {/*Project Item*/}
+                <div class="projects-item">
+                <div class="projects-item-info">
+                    <h1>Project 1</h1>
+                    <h2>Subtitulo</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae vel, eum quia delectus quas similique sunt illum porro nostrum dolores quos dicta possimus corporis, asperiores molestias nobis ratione omnis distinctio.</p>
+                </div>
+                <div class="projects-item-img">
+                    <img src="./img/img-1.png" alt="" />
+                </div>
+                </div>
+
+                {/*Project Item*/}
+                <div class="projects-item">
+                <div class="projects-item-info">
+                    <h1>Project 2</h1>
+                    <h2>Subtitulo</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae vel, eum quia delectus quas similique sunt illum porro nostrum dolores quos dicta possimus corporis, asperiores molestias nobis ratione omnis distinctio.</p>
+                </div>
+                <div class="projects-item-img">
+                    <img src="./img/img-1.png" alt="" />
+                </div>
+                </div>
+
+                {/*Project Item*/}
+                <div class="projects-item">
+                <div class="projects-item-info">
+                    <h1>Project 3</h1>
+                    <h2>Subtitulo</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae vel, eum quia delectus quas similique sunt illum porro nostrum dolores quos dicta possimus corporis, asperiores molestias nobis ratione omnis distinctio.</p>
+                </div>
+                <div class="projects-item-img">
+                    <img src="./img/img-1.png" alt="" />
+                </div>
+                </div>
+
+                {/*Project Item*/}
+                <div class="projects-item">
+                <div class="projects-item-info">
+                    <h1>Project 4</h1>
+                    <h2>Subtitulo</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae vel, eum quia delectus quas similique sunt illum porro nostrum dolores quos dicta possimus corporis, asperiores molestias nobis ratione omnis distinctio.</p>
+                </div>
+                <div class="projects-item-img">
+                    <img src="./img/img-1.png" alt="" />
+                </div>
+                </div>
+
+                {/*Project Item*/}
+                <div class="projects-item">
+                <div class="projects-item-info">
+                    <h1>Project 5</h1>
+                    <h2>Subtitulo</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae vel, eum quia delectus quas similique sunt illum porro nostrum dolores quos dicta possimus corporis, asperiores molestias nobis ratione omnis distinctio.</p>
+                </div>
+                <div class="projects-item-img">
+                    <img src="./img/img-1.png" alt="" />
+                </div>
+                </div>
+
+            </div>
+
+            </div>
+            </section>
+        </div>
+    )
+}
+
+export default Projects
