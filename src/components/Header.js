abf815155d48597dcd89c536dca29b8db70c0b4f
jhed <gallegosmatiasjoel@gmail.com>
2021-03-10 12:31:05 -0300

    Fixed Hamburger Nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from 'react';
 function Header() {
 
     const [show, handleShow] = useState(false);
+    const [navbar, setNavbar] = useState(false);
+
+    const showNavbar = () => setNavbar(!navbar);
 
 	useEffect(() => {
 		window.addEventListener('scroll', () => {
@@ -27,19 +30,19 @@ function Header() {
                     <a href="#hero"><h1><span>MATIAS</span> <span></span>GALLEGOS</h1></a>
                     </div>
 
-                    <div className="navbar">
+                    <div className={navbar ? 'navbar active' : 'navbar'}>
 
-                    {/*Hambuger*/}
-                    <div className="hamburger"><div className="bar"></div></div>
+                        {/*Hambuger*/}
+                        <div className="hamburger" onClick={showNavbar}><div className="bar"></div></div>
 
-                    {/*navbar*/}
-                    <ul>
-                        <li><a href="#hero" data-after="Inicio">Inicio</a></li>
-                        <li><a href="#about" data-after="SobreMí">Sobre Mí</a></li>
-                        <li><a href="#services" data-after="Servicios">Servicios</a></li>
-                        <li><a href="#projects" data-after="Proyectos">Proyectos</a></li>
-                        <li><a href="#contact" data-after="Contacto">Contacto</a></li>
-                    </ul>
+                        {/*navbar*/}
+                        <ul className={navbar ? 'burger active' : 'burger'} onClick={showNavbar}>
+                            <li><a href="#hero" data-after="Inicio">Inicio</a></li>
+                            <li><a href="#about" data-after="SobreMí">Sobre Mí</a></li>
+                            <li><a href="#services" data-after="Servicios">Servicios</a></li>
+                            <li><a href="#projects" data-after="Proyectos">Proyectos</a></li>
+                            <li><a href="#contact" data-after="Contacto">Contacto</a></li>
+                        </ul>
 
                     </div>
                 </div>
